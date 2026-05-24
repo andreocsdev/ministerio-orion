30c1ea8c6863b0ca00f41e3559044f39c095ed44
André <andreocs.dev@gmail.com>
2026-05-24 19:20:26 -0300

    orion vercel

--- app/api/groups/add-users/route.ts
+++ app/api/groups/add-users/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import { z } from "zod";
-import { AddUserToGroup } from "../../../src/server/usecases/AddUserToGroup";
+import { AddUserToGroup } from "../../../../src/server/usecases/AddUserToGroup";
 
 const Schema = z.object({
   groupId: z.string(),
